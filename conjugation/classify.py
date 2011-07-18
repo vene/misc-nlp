324bbc1ea5894a3c95883f6be68a05b865df7235
vene <vlad@vene.ro>
2011-07-19 00:28:34 +0300

    doar cu ta

--- conjugation/classify.py
+++ conjugation/classify.py
@@ -3,11 +3,12 @@ import sys
 import numpy as np
 
 from scikits.learn.svm.sparse import SVC
+from scikits.learn.svm.libsvm import set_verbosity_wrap
 from scikits.learn.linear_model.sparse import LogisticRegression
 from preprocess import get_clf, load_data
 from scikits.learn.metrics import classification_report
 from scikits.learn.cross_val import StratifiedKFold
-
+from scikits.learn.grid_search import GridSearchCV
 if len(sys.argv) < 2:
     filename = 'inf-all-labeled.txt'
 else:
@@ -16,8 +17,14 @@ else:
 X, y = load_data(filename)
 
 np.random.seed(42)
-train, test = iter(StratifiedKFold(y, 2)).next()
-clf = get_clf(n=3, clf=LogisticRegression(C=10))
-clf.fit(X[train], y[train])
-print clf.score(X[test], y[test])
-print classification_report(clf.predict(X[test]), y[test])
+set_verbosity_wrap(1)
+clf = get_clf(n=3, clf=SVC(kernel='linear'))
+grid = GridSearchCV(clf, {'clf__C': np.logspace(-4, 10, num=15)},
+                    cv=StratifiedKFold(y, k=2))
+grid.fit(X, y)
+print grid.best_score, grid.best_estimator
+#for train, test in StratifiedKFold(y, 2):
+#    clf = get_clf(n=3, clf=SVC())
+#    clf.fit(X[train], y[train])
+#    print clf.score(X[test], y[test])
+ #print classification_report(clf.predict(X[test]), y[test])
--- conjugation/new_labeling.py
+++ conjugation/new_labeling.py
@@ -12,16 +12,10 @@ import codecs
 import numpy as np
 
 f = codecs.open('verbe-indprez.txt', 'r', encoding='utf-8-sig')
-labeled = codecs.open('inf-all-labeled.txt', 'w', encoding='utf-8')
+labeled = codecs.open('inf-ta-labeled.txt', 'w', encoding='utf-8')
 rules = []
 # conj 1
 # a spera
-rules.append({'1sg': u'^([a-zăâîşţ]+)$',
-              '2sg': u'^([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)ă$',
-              '1pl': u'^([a-zăâîşţ]+)ăm$',
-              '2pl': u'^([a-zăâîşţ]+)aţi$',
-              '3pl': u'^([a-zăâîşţ]+)ă$'})
               
 # pt verbe cu -ta gen cânta
 # (alternanta in flectiv t->ţ la 2sg)
@@ -32,48 +26,87 @@ rules.append({'1sg': u'^([a-zăâîşţ]+)t$',
               '2pl': u'^([a-zăâîşţ]+)taţi$',
               '3pl': u'^([a-zăâîşţ]+)tă$'})
 
-# pt verbe gen număra
-# (alternanta ă-> e la 2sg)
-rules.append({'1sg': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)$',
-             '2sg': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)i$',
-             '3sg': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)ă$',
-             '1pl': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)ăm$',
-             '2pl': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)aţi$',
-             '3pl': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)ă$'})
-
-# pt verbe cu tr, pl, bl, fl (intra)
-rules.append({'1sg': u'^([a-zăâîşţ]+)u$',
-              '2sg': u'^([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)ă$',
-              '1pl': u'^([a-zăâîşţ]+)ăm$',
-              '2pl': u'^([a-zăâîşţ]+)aţi$',
-              '3pl': u'^([a-zăâîşţ]+)ă$'})
-
-# pt verbe care se termina in şca- mişca
-rules.append({'1sg': u'^([a-zăâîşţ]+)şc$',
+# (alternanta in flectiv t->ţ la 2sg)
+rules.append({'1sg': u'^([a-zăâîşţ]+)st$',
               '2sg': u'^([a-zăâîşţ]+)şti$',
-              '3sg': u'^([a-zăâîşţ]+)şcă$',
-              '1pl': u'^([a-zăâîşţ]+)şcăm$',
-              '2pl': u'^([a-zăâîşţ]+)şcaţi$',
-              '3pl': u'^([a-zăâîşţ]+)şcă$'})
+              '3sg': u'^([a-zăâîşţ]+)stă$',
+              '1pl': u'^([a-zăâîşţ]+)stăm$',
+              '2pl': u'^([a-zăâîşţ]+)staţi$',
+              '3pl': u'^([a-zăâîşţ]+)stă$'})
+              
+# a destepta (alternanta in flectiv t->ţ la 2sg + e-ea )
+rules.append({'1sg': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)t$',
+              '2sg': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)ţi$',
+              '3sg': u'^([a-zăâîşţ]+)ea([a-zăâîşţ]+)tă$',
+              '1pl': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)tăm$',
+              '2pl': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)taţi$',
+              '3pl': u'^([a-zăâîşţ]+)ea([a-zăâîşţ]+)tă$'})
 
-# pt verbe gen taia (cu ia si vocala inainte)
-rules.append({'1sg': u'^([a-zăâîşţ]+)$',
-              '2sg': u'^([a-zăâîşţ]+)$',
-              '3sg': u'^([a-zăâîşţ]+)e$',
-              '1pl': u'^([a-zăâîşţ]+)em$',
-              '2pl': u'^([a-zăâîşţ]+)aţi$',
-              '3pl': u'^([a-zăâîşţ]+)e$'})
+# a reprezenta
+rules.append({'1sg': u'^([a-zăâîşţ]+)i([a-zăâîşţ]+)t$',
+              '2sg': u'^([a-zăâîşţ]+)i([a-zăâîşţ]+)ţi$',
+              '3sg': u'^([a-zăâîşţ]+)i([a-zăâîşţ]+)tă$',
+              '1pl': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)tăm$',
+              '2pl': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)taţi$',
+              '3pl': u'^([a-zăâîşţ]+)i([a-zăâîşţ]+)tă$'})
+              
+# a destepta (alternanta in flectiv t->ţ la 2sg + e-ea ) deșerta
+rules.append({'1sg': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)t$',
+              '2sg': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)ţi$',
+              '3sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)tă$',
+              '1pl': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)tăm$',
+              '2pl': u'^([a-zăâîşţ]+)e([a-zăâîşţ]+)taţi$',
+              '3pl': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)tă$'})
 
-# pt verbe gen speria (cu ia si consoana inainte)
-rules.append({'1sg': u'^([a-zăâîşţ]+)i$',
-              '2sg': u'^([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)e$',
-              '1pl': u'^([a-zăâîşţ]+)em$',
-              '2pl': u'^([a-zăâîşţ]+)aţi$',
-              '3pl': u'^([a-zăâîşţ]+)e$'})
+# a tresălta
+rules.append({'1sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)t$',
+              '2sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)ţi$',
+              '3sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)tă$',
+              '1pl': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)tăm$',
+              '2pl': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)taţi$',
+              '3pl': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)tă$'})
+              
+# (alternanta in flectiv t->ţ la 2sg + o-oa-u)
+rules.append({'1sg': u'^([a-zăâîşţ]+)o([a-zăâîşţ]+)t$',
+              '2sg': u'^([a-zăâîşţ]+)o([a-zăâîşţ]+)ţi$',
+              '3sg': u'^([a-zăâîşţ]+)oa([a-zăâîşţ]+)tă$',
+              '1pl': u'^([a-zăâîşţ]+)u([a-zăâîşţ]+)tăm$',
+              '2pl': u'^([a-zăâîşţ]+)u([a-zăâîşţ]+)taţi$',
+              '3pl': u'^([a-zăâîşţ]+)oa([a-zăâîşţ]+)tă$'})
 
-# pt verbe care se conjuga cu ez (dansa)  (l-ai pus deja)
+# (alternanta in flectiv t->ţ la 2sg + o-oa-u)
+rules.append({'1sg': u'^([a-zăâîşţ]+)ot$',
+              '2sg': u'^([a-zăâîşţ]+)oţi$',
+              '3sg': u'^([a-zăâîşţ]+)oată$',
+              '1pl': u'^([a-zăâîşţ]+)otăm$',
+              '2pl': u'^([a-zăâîşţ]+)otaţi$',
+              '3pl': u'^([a-zăâîşţ]+)oată$'})
+
+# (alternanta in flectiv t->ţ la 2sg + căpăta)
+rules.append({'1sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)ăt$',
+              '2sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)eţi$',
+              '3sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)ătă$',
+              '1pl': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)ătăm$',
+              '2pl': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)ătaţi$',
+              '3pl': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)ătă$'})
+
+# (alternanta in flectiv t->ţ la 2sg + ă-e-a)
+rules.append({'1sg': u'^([a-zăâîşţ]+)ăt$',
+              '2sg': u'^([a-zăâîşţ]+)eţi$',
+              '3sg': u'^([a-zăâîşţ]+)ată$',
+              '1pl': u'^([a-zăâîşţ]+)ătăm$',
+              '2pl': u'^([a-zăâîşţ]+)ătaţi$',
+              '3pl': u'^([a-zăâîşţ]+)ată$'})
+
+# (alternanta in flectiv t->ţ la 2sg + ă-e-a)
+rules.append({'1sg': u'^([a-zăâîşţ]+)ăt$',
+              '2sg': u'^([a-zăâîşţ]+)ăţi$',
+              '3sg': u'^([a-zăâîşţ]+)ată$',
+              '1pl': u'^([a-zăâîşţ]+)ătăm$',
+              '2pl': u'^([a-zăâîşţ]+)ătaţi$',
+              '3pl': u'^([a-zăâîşţ]+)ată$'})
+              
+# pt verbe care se conjuga cu ez (cita)  (l-ai pus deja)
 rules.append({'1sg': u'^([a-zăâîşţ]+)ez$',
               '2sg': u'^([a-zăâîşţ]+)ezi$',
               '3sg': u'^([a-zăâîşţ]+)ează$',
@@ -81,121 +114,20 @@ rules.append({'1sg': u'^([a-zăâîşţ]+)ez$',
               '2pl': u'^([a-zăâîşţ]+)aţi$',
               '3pl': u'^([a-zăâîşţ]+)ează$'})
 
-# pt verbe care se conjuga cu ez si au ia la inf (copia) (l-ai pus deja)
-rules.append({'1sg': u'^([a-zăâîşţ]+)ez$',
-              '2sg': u'^([a-zăâîşţ]+)ezi$',
-              '3sg': u'^([a-zăâîşţ]+)ază$',
-              '1pl': u'^([a-zăâîşţ]+)em$',
+# a consta
+rules.append({'1sg': u'^([a-zăâîşţ]+)au$',
+              '2sg': u'^([a-zăâîşţ]+)ai$',
+              '3sg': u'^([a-zăâîşţ]+)ă$',
+              '1pl': u'^([a-zăâîşţ]+)ăm$',
               '2pl': u'^([a-zăâîşţ]+)aţi$',
-              '3pl': u'^([a-zăâîşţ]+)ază$'})
-
-# pt verbe cu ca la inf (parca, şoca) si ez
-rules.append({'1sg': u'^([a-zăâîşţ]+)chez$',
-              '2sg': u'^([a-zăâîşţ]+)chezi$',
-              '3sg': u'^([a-zăâîşţ]+)chează$',
-              '1pl': u'^([a-zăâîşţ]+)căm$',
-              '2pl': u'^([a-zăâîşţ]+)caţi$',
-              '3pl': u'^([a-zăâîşţ]+)chează$'})
-
-# conj a 2a (infinitivul in -ea)
-# ->la fel ca primul pattern de la conj a 3a
-
-# pt verbele care au alternanta ă->a in radical
-rules.append({'1sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)$',
-              '2sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)e$',
-              '1pl': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)em$',
-              '2pl': u'^([a-zăâîşţ]+)ă([a-zăâîşţ]+)eţi$',
-              '3pl': u'^([a-zăâîşţ]+)a([a-zăâîşţ]+)$'})
-
-# conj a 3a (infinitivul in -e)
-
-rules.append({'1sg': u'^([a-zăâîşţ]+)$',
-              '2sg': u'^([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)e$',
-              '1pl': u'^([a-zăâîşţ]+)em$',
-              '2pl': u'^([a-zăâîşţ]+)eţi$',
-              '3pl': u'^([a-zăâîşţ]+)$'})
-
-# a scrie
-rules.append({'1sg': u'^([a-zăâîşţ]+)u$',
-              '2sg': u'^([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)e$',
-              '1pl': u'^([a-zăâîşţ]+)em$',
-              '2pl': u'^([a-zăâîşţ]+)eţi$',
-              '3pl': u'^([a-zăâîşţ]+)u$'})
-
-# pt -şte la infinitiv (naşte, paşte)(l-ai pus deja)
-rules.append({'1sg': u'^([a-zăâîşţ]+)sc$',
-              '2sg': u'^([a-zăâîşţ]+)şti$',
-              '3sg': u'^([a-zăâîşţ]+)şte$',
-              '1pl': u'^([a-zăâîşţ]+)ştem$',
-              '2pl': u'^([a-zăâîşţ]+)şteţi$',
-              '3pl': u'^([a-zăâîşţ]+)sc$'})
-
-# pt -ne la infinitiv (l-ai pus deja) ex: a pune
-rules.append({'1sg': u'^([a-zăâîşţ]+)n$', 
-              '2sg': u'^([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)ne$',
-              '1pl': u'^([a-zăâîşţ]+)nem$',
-              '2pl': u'^([a-zăâîşţ]+)neţi$',
-              '3pl': u'^([a-zăâîşţ]+)n$'})
-
-# conj a 4a
-
-# pt verbe ca a fugi
-rules.append({'1sg': u'^([a-zăâîşţ]+)$',
-              '2sg': u'^([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)e$',
-              '1pl': u'^([a-zăâîşţ]+)im$',
-              '2pl': u'^([a-zăâîşţ]+)iţi$',
-              '3pl': u'^([a-zăâîşţ]+)$'})
-
-# pt verbe cu ui, âi, ăi (sui)
-rules.append({'1sg': u'^([a-zăâîşţ]+)i$',
-              '2sg': u'^([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)ie$',
-              '1pl': u'^([a-zăâîşţ]+)im$',
-              '2pl': u'^([a-zăâîşţ]+)iţi$',
-              '3pl': u'^([a-zăâîşţ]+)ie$'})
-
-# pt verbe care se conjuga cu esc (vorbi, citi) (l-ai pus deja)
-rules.append({'1sg': u'^([a-zăâîşţ]+)esc$',
-              '2sg': u'^([a-zăâîşţ]+)eşti$',
-              '3sg': u'^([a-zăâîşţ]+)eşte$',
-              '1pl': u'^([a-zăâîşţ]+)im$',
-              '2pl': u'^([a-zăâîşţ]+)iţi$',
-              '3pl': u'^([a-zăâîşţ]+)esc$'})
-
-# pt verbe cu esc care se termina in ui, ăi, oi(locui)
-rules.append({'1sg': u'^([a-zăâîşţ]+)iesc$',
-              '2sg': u'^([a-zăâîşţ]+)ieşti$',
-              '3sg': u'^([a-zăâîşţ]+)ieşte$',
-              '1pl': u'^([a-zăâîşţ]+)im$',
-              '2pl': u'^([a-zăâîşţ]+)iţi$',
-              '3pl': u'^([a-zăâîşţ]+)iesc$'})
-
-#?pt verbele care se termina cu î la inf (omorî) 
-#(problema: avem alternanta o->oa la 3)
-rules.append({'1sg': u'^([a-zăâîşţ]+)o([a-zăâîşţ]+)$',
-              '2sg': u'^([a-zăâîşţ]+)o([a-zăâîşţ]+)i$',
-              '3sg': u'^([a-zăâîşţ]+)oa([a-zăâîşţ]+)ă$',
-              '1pl': u'^([a-zăâîşţ]+)o([a-zăâîşţ]+)âm$',
-              '2pl': u'^([a-zăâîşţ]+)o([a-zăâîşţ]+)âţi$',
-              '3pl': u'^([a-zăâîşţ]+)oa([a-zăâîşţ]+)ă$'})
-
-# pt verbele cu î si ăsc (hotărî)
-rules.append({'1sg': u'^([a-zăâîşţ]+)ăsc$',
-              '2sg': u'^([a-zăâîşţ]+)ăşti$',
-              '3sg': u'^([a-zăâîşţ]+)ăşte$',
-              '1pl': u'^([a-zăâîşţ]+)âm$',
-              '2pl': u'^([a-zăâîşţ]+)âţi$',
-              '3pl': u'^([a-zăâîşţ]+)ăsc$'})
+              '3pl': u'^([a-zăâîşţ]+)ă$'})
 
 words = {}
 
 for line in f:
     word, base, persoana = line.split()
+    if not base.endswith('ta'):
+        continue
     persoana = persoana.split(".")[3]
     if words.has_key(base):
         words[base].append((word, persoana))  # 3pl
@@ -216,22 +148,22 @@ def check(forms, rules):
 
     return len(pers) >= 6
 
-
-count = np.zeros(len(rules))
-uncaptured = 0
+totals = len(words)
+count = np.zeros(len(rules) + 1)
 for base, forms in words.items():
-    label_ez = 0
-    label_esc = 0
+    if len(forms) < 6:
+        totals -= 1
+        continue
     flags, = np.where([check(forms, rls) for rls in rules])
-    
+    flags  += 1
     matches = len(flags)
     if matches > 1:
         print base, flags
     if matches == 0:
-        uncaptured += 1
+        count[0] += 1
         label = 0
     else:
-        label = 1 + flags[0]  # index of the first match 
+        label = flags[0]  # index of the first match 
 
     count[flags] += 1
 
@@ -240,6 +172,6 @@ for base, forms in words.items():
 print "Captured: "
 for i, n in enumerate(count):
     print '%d: %d' % (i, n)
-print "Uncaptured: %d out of %d" % (uncaptured, len(words))
+print "Uncaptured: %d out of %d" % (count[0], totals)
 for fl in (f, labeled): 
     fl.close()
