9f5edc1a1eeff4ec1369182630834f7e368415fc
vene <vlad@vene.ro>
2011-10-08 22:16:14 +0300

    cleanup

--- preprocess.py
+++ preprocess.py
@@ -6,27 +6,50 @@ Created on Wed Apr 13 18:13:21 2011
 @author: vene
 """
 
-from scikits.learn.feature_extraction import text
+from sklearn.feature_extraction.text import CharNGramAnalyzer, CountVectorizer
+from sklearn.preprocessing import Binarizer
+from scikits.learn.pipeline import Pipeline
+from scikits.learn import naive_bayes
+import codecs
+
 import numpy as np
 
 class SimplePreprocessor(object):
-    """Fast preprocessor suitable for roman languages"""
-
-    # the space at the end = tiny hax for skl, will fix later
     def preprocess(self, unicode_text):
-        return unicode(unicode_text.strip().lower() + self.suffix + " ")
+        return unicode(unicode_text.strip().lower() + self.suffix)
         
     def __init__(self, suffix=''):
         self.suffix = suffix
 
 
-def extract_features(words, n, count=True):
-    pp = SimplePreprocessor()
-    analyzer = text.CharNGramAnalyzer(min_n=1, max_n=n, preprocessor=pp)
-    vectorizer = text.CountVectorizer(analyzer=analyzer, max_df=None)
-    
-    transformed_words = vectorizer.fit_transform(words).toarray()
-    transformed_words = np.array(transformed_words, dtype=np.float)
-    if not count:
-        transformed_words[transformed_words > 0] = 1.0
-    return transformed_words, vectorizer
+def get_clf(n=3, binarize=True, clf=None):
+    steps = [('vectorizer', CountVectorizer(CharNGramAnalyzer(min_n=1, max_n=n,
+                                          preprocessor=SimplePreprocessor())))]
+    if binarize:
+        steps.append(('binarizer', Binarizer(copy=False)))
+        if not clf:
+            clf = naive_bayes.BernoulliNB()
+    elif not clf:
+        clf = naive_bayes.MultinomialNB()
+    steps.append(('clf', clf))
+    return Pipeline(steps)
+
+
+def preprocess_data(X, n=3, binarize=True):
+    cv = CountVectorizer(CharNGramAnalyzer(min_n=1, max_n=n,
+                                           preprocessor=SimplePreprocessor()))
+    X = cv.fit_transform(X)
+    if binarize:
+        X = Binarizer(copy=False).transform(X)
+    return X
+
+
+def load_data(filename='inf-ta-labeled.txt'):
+    infinitives, y = [], []
+    with codecs.open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            inf, label = line.split()
+            infinitives.append(inf)
+            y.append(int(label))
+    infinitives, y = np.array(infinitives), np.array(y, dtype=np.int)
+    return infinitives, y
