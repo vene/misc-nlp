87c7de4552d04bc5861c8330158ccaac91df4ec5
Vlad Niculae <vlad@vene.ro>
2012-03-27 17:29:28 +0300

    Update train size experiment

--- substantive/classify.py
+++ substantive/classify.py
@@ -2,97 +2,94 @@ import cPickle
 import numpy as np
 
 from sklearn.preprocessing import Binarizer
-from sklearn.cross_validation import KFold
-from sklearn.grid_search import GridSearchCV
-from sklearn.svm import LinearSVC, SVC
-from sklearn.metrics import classification_report
+from sklearn.svm import LinearSVC
 
 import preprocess
 
 if __name__ == '__main__':
-	X_sg, y_sg = preprocess.load_data('singular.txt')
-	X_pl, y_pl = preprocess.load_data('plural.txt')
+    X_sg, y_sg = preprocess.load_data('singular.txt')
+    X_pl, y_pl = preprocess.load_data('plural.txt')
 
-	X_sg_p, v_sg = preprocess.preprocess_data(X_sg, suffix='$', n=5,
-											  return_vect=True, binarize=False)
-	X_pl_p, v_pl = preprocess.preprocess_data(X_pl, suffix='$', n=5,
-	                                          return_vect=True, binarize=False)
+    X_sg_p, v_sg = preprocess.preprocess_data(X_sg, suffix='$', n=5,
+                                              return_vect=True, binarize=False)
+    X_pl_p, v_pl = preprocess.preprocess_data(X_pl, suffix='$', n=5,
+                                              return_vect=True, binarize=False)
 
-	try:
-		pkl = open('svc_sg.pkl', 'r')
-		print 'Stored singular model found, loading...'
-		clf = cPickle.load(pkl)
-		pkl.close()
-	except IOError:
-		clf = LinearSVC(C=0.1, scale_C=False).fit(X_sg_p, y_sg)
-		sg_model = open('svc_sg.pkl', 'wb')
-		cPickle.dump(clf, sg_model)
-		sg_model.close()
+    try:
+        pkl = open('svc_sg.pkl', 'r')
+        print 'Stored singular model found, loading...'
+        clf = cPickle.load(pkl)
+        pkl.close()
+    except IOError:
+        clf = LinearSVC(C=0.1, scale_C=False).fit(X_sg_p, y_sg)
+        sg_model = open('svc_sg.pkl', 'wb')
+        cPickle.dump(clf, sg_model)
+        sg_model.close()
 
-	print 'Loading neutral data...'
-	X_sg_n_clean = preprocess.load_data('singular_n.txt', labels=False)
-	X_sg_n = Binarizer(copy=False).transform(v_sg.transform(X_sg_n_clean))
-	print 'Predicting...'
-	y_sg_n = clf.predict(X_sg_n)
-	print (y_sg_n == 0).mean()
+    print 'Loading neutral data...'
+    X_sg_n_clean = preprocess.load_data('singular_n.txt', labels=False)
+    X_sg_n = Binarizer(copy=False).transform(v_sg.transform(X_sg_n_clean))
+    print 'Predicting...'
+    y_sg_n = clf.predict(X_sg_n)
+    print (y_sg_n == 0).mean()
 
-	try:
-		pkl = open('svc_pl.pkl', 'r')
-		print 'Stored plural model found, loading...'
-		clf = cPickle.load(pkl)
-		pkl.close()
-	except IOError:
-		weights = [x.strip().endswith("uri") for x in X_pl]
-		weights = 1 + np.array(weights, dtype=np.float)
-		clf = LinearSVC(C=0.1, scale_C=False)
-		clf.fit(X_pl_p, y_pl)
-		pl_model = open('svc_pl.pkl', 'wb')
-		cPickle.dump(clf, pl_model)
-		pl_model.close()
-	print 'Loading neutral data...'
-	X_pl_n_clean = preprocess.load_data('plural_n.txt', labels=False)
-	X_pl_n = Binarizer(copy=False).transform(v_pl.transform(X_pl_n_clean))
-	print 'Predicting...'
-	y_pl_n = clf.predict(X_pl_n)
-	print (y_pl_n == 1).mean()
+    try:
+        pkl = open('svc_pl.pkl', 'r')
+        print 'Stored plural model found, loading...'
+        clf = cPickle.load(pkl)
+        pkl.close()
+    except IOError:
+        weights = [x.strip().endswith("uri") for x in X_pl]
+        weights = 1 + np.array(weights, dtype=np.float)
+        clf = LinearSVC(C=0.1, scale_C=False)
+        clf.fit(X_pl_p, y_pl)
+        pl_model = open('svc_pl.pkl', 'wb')
+        cPickle.dump(clf, pl_model)
+        pl_model.close()
+    print 'Loading neutral data...'
+    X_pl_n_clean = preprocess.load_data('plural_n.txt', labels=False)
+    X_pl_n = Binarizer(copy=False).transform(v_pl.transform(X_pl_n_clean))
+    print 'Predicting...'
+    y_pl_n = clf.predict(X_pl_n)
+    print (y_pl_n == 1).mean()
 
-	mf = np.logical_and((y_sg_n == 0), (y_pl_n == 1))
-	fm = np.logical_and((y_sg_n == 1), (y_pl_n == 0))
-	mm = np.logical_and((y_sg_n == 0), (y_pl_n == 0))
-	ff = np.logical_and((y_sg_n == 1), (y_pl_n == 1))
+    mf = np.logical_and((y_sg_n == 0), (y_pl_n == 1))
+    fm = np.logical_and((y_sg_n == 1), (y_pl_n == 0))
+    mm = np.logical_and((y_sg_n == 0), (y_pl_n == 0))
+    ff = np.logical_and((y_sg_n == 1), (y_pl_n == 1))
 
 
-	print ".\tf\tm\nm\t%d\t%d\nf\t%d\t%d" % (
-		mf.sum(), mm.sum(), ff.sum(), fm.sum())
+    print ".\tf\tm\nm\t%d\t%d\nf\t%d\t%d" % (
+        mf.sum(), mm.sum(), ff.sum(), fm.sum())
 
-	print ".\tf\tm\nm\t%.4f\t%.4f\nf\t%.4f\t%.4f" % (
-		mf.mean(), mm.mean(), ff.mean(), fm.mean())
-	
-	def print_word(i):
-		print X_sg_n_clean[i].strip(), '/', X_pl_n_clean[i].strip()
-	
-	liniute_sg = liniute_pl = uri_pl_gresite = uri_pl_corecte = 0
-	print "Words completely misclassified:"
-	for i in np.flatnonzero(fm):
-		print_word(i)
-	print "\n\n\nWords with misclassified singular:"
-	for i in np.flatnonzero(ff):
-		if "-" in X_sg_n_clean[i]: 
-			liniute_sg += 1
-		print_word(i)
-	print "\n\n\nWords with misclassified plural:"
-	for i in np.flatnonzero(mm):
-		if "-" in X_sg_n_clean[i]:
-			liniute_pl += 1
-		print_word(i)
+    print ".\tf\tm\nm\t%.4f\t%.4f\nf\t%.4f\t%.4f" % (
+        mf.mean(), mm.mean(), ff.mean(), fm.mean())
+    
+    def print_word(i):
+        print X_sg_n_clean[i].strip(), '/', X_pl_n_clean[i].strip()
+    
+    liniute_sg = liniute_pl = uri_pl_gresite = uri_pl_corecte = 0
+    print "Words completely misclassified:"
+    for i in np.flatnonzero(fm):
+        print_word(i)
+    print "\n\n\nWords with misclassified singular:"
+    for i in np.flatnonzero(ff):
+        if "-" in X_sg_n_clean[i]: 
+            liniute_sg += 1
+        print_word(i)
+    print "\n\n\nWords with misclassified plural:"
+    for i in np.flatnonzero(mm):
+        if "-" in X_sg_n_clean[i]:
+            liniute_pl += 1
+        print_word(i)
 
-	for i in np.flatnonzero(y_pl_n == 1):
-		if X_pl_n_clean[i].strip()[-3:] == 'uri':
-			uri_pl_corecte += 1
-	for i in np.flatnonzero(y_pl_n == 0):
-		if X_pl_n_clean[i].strip()[-3:] == 'uri':
-			uri_pl_gresite += 1
+    for i in np.flatnonzero(y_pl_n == 1):
+        if X_pl_n_clean[i].strip()[-3:] == 'uri':
+            uri_pl_corecte += 1
+    for i in np.flatnonzero(y_pl_n == 0):
+        if X_pl_n_clean[i].strip()[-3:] == 'uri':
+            uri_pl_gresite += 1
 
-	print "Dashes in misclassified sg: %d" % liniute_sg
-	print "Dashes in misclassified pl: %d" % liniute_pl
-	print "-uri plurals: correct %d, incorrect %d" % (uri_pl_corecte, uri_pl_gresite)
+    print "Dashes in misclassified sg: %d" % liniute_sg
+    print "Dashes in misclassified pl: %d" % liniute_pl
+    print "-uri plurals: correct %d, incorrect %d" % (uri_pl_corecte, uri_pl_gresite)
--- substantive/train_size.py
+++ substantive/train_size.py
@@ -1,4 +1,3 @@
-import cPickle
 import numpy as np
 
 from sklearn.preprocessing import Binarizer
@@ -9,80 +8,92 @@ from sklearn.externals.joblib import Parallel, delayed
 
 import preprocess
 
-def nouns_score(X, y, X_test, expected):
-	clf = LinearSVC(C=0.1, scale_C=False)
-	return np.mean(clf.fit(X, y).predict(X_test) == expected)
+
+def nouns_score(X_sg, y_sg, X_pl, y_pl, X_sg_test, X_pl_test):
+    sg_clf = LinearSVC(C=0.1, scale_C=False).fit(X_sg, y_sg)
+    pl_clf = LinearSVC(C=0.1, scale_C=False).fit(X_pl, y_pl)
+    sg_pred = sg_clf.predict(X_sg_test)
+    pl_pred = pl_clf.predict(X_pl_test)
+    sg_score = np.mean(sg_pred == 0)
+    pl_score = np.mean(pl_pred == 1)
+    overall_score = np.mean(np.logical_and(sg_pred == 0, pl_pred == 1))
+    return sg_score, pl_score, overall_score
 
 
-def sg_score(X, y, X_test): 
-	return nouns_score(X, y, X_test, 0)
+def sg_score(X, y, X_test):
+    return nouns_score(X, y, X_test, 0)
 
 
-def pl_score(X, y, X_test): 
-	return nouns_score(X, y, X_test, 1)
+def pl_score(X, y, X_test):
+    return nouns_score(X, y, X_test, 1)
 
 
 def plot(scores):
-	import matplotlib.pylab as pl
-	from matplotlib.ticker import FuncFormatter
+    import matplotlib.pylab as pl
+    from matplotlib.ticker import FuncFormatter
 
-	def percentages(x, pos=0):
-		return '%2.2f%%'%(100*x)
+    def percentages(x, pos=0):
+        return '%2.2f%%' % (100 * x)
 
-	ax1 = pl.subplot(211)
-	pl.errorbar(scores[:, 1], scores[:, 2], yerr=scores[:, 3],
-				c='k', marker='o')
-	pl.ylabel("Singular neuters as masculine")
-	ax1.yaxis.set_major_formatter(FuncFormatter(percentages))
-	ax2 = pl.subplot(212, sharex=ax1)
-	pl.errorbar(scores[:, 1], scores[:, 4], yerr=scores[:, 5],
-			    c='k', marker='o')
+    ax1 = pl.subplot(311)
+    pl.errorbar(scores[:, 1], scores[:, 2], yerr=scores[:, 5],
+                c='k', marker='o')
+    pl.ylabel("Singular accuracy")
+    ax1.yaxis.set_major_formatter(FuncFormatter(percentages))
 
-	pl.setp(ax2.get_xticklabels(), visible=False)
-	pl.ylabel("Plural neuters as feminine")
-	ax2.yaxis.set_major_formatter(FuncFormatter(percentages))
-	pl.xlabel("Proportion of training set used")
-	for ext in ('pdf', 'svg', 'png'):
-		pl.savefig('train_size.%s' % ext)
+    ax2 = pl.subplot(312, sharex=ax1)
+    pl.errorbar(scores[:, 1], scores[:, 3], yerr=scores[:, 6],
+                c='k', marker='o')
+
+    pl.ylabel("Plural accuracy")
+    ax2.yaxis.set_major_formatter(FuncFormatter(percentages))
+
+    ax3 = pl.subplot(313, sharex=ax2)
+    pl.errorbar(scores[:, 1], scores[:, 4], yerr=scores[:, 7],
+                c='k', marker='o')
+    pl.ylabel("Overall accuracy")
+    ax3.yaxis.set_major_formatter(FuncFormatter(percentages))
+    pl.setp(ax3.get_xticklabels(), visible=False)
+    pl.xlabel("Proportion of training set used")
+
+    for ext in ('pdf', 'svg', 'png'):
+        pl.savefig('train_size.%s' % ext)
 
 
 if __name__ == '__main__':
-	print 'Loading training and test data...'
-	X_sg, y_sg = preprocess.load_data('singular.txt')
-	X_pl, y_pl = preprocess.load_data('plural.txt')
+    print 'Loading training and test data...'
+    X_sg, y_sg = preprocess.load_data('singular.txt')
+    X_pl, y_pl = preprocess.load_data('plural.txt')
 
-	X_sg_p, v_sg = preprocess.preprocess_data(X_sg, suffix='$', n=5,
-											  return_vect=True, binarize=False)
-	X_pl_p, v_pl = preprocess.preprocess_data(X_pl, suffix='$', n=5,
-	                                          return_vect=True, binarize=False)
+    X_sg_p, v_sg = preprocess.preprocess_data(X_sg, suffix='$', n=5,
+                                              return_vect=True, binarize=False)
+    X_pl_p, v_pl = preprocess.preprocess_data(X_pl, suffix='$', n=5,
+                                              return_vect=True, binarize=False)
 
+    X_sg_n_clean = preprocess.load_data('singular_n.txt', labels=False)
+    X_sg_n = Binarizer(copy=False).transform(v_sg.transform(X_sg_n_clean))
 
-	X_sg_n_clean = preprocess.load_data('singular_n.txt', labels=False)
-	X_sg_n = Binarizer(copy=False).transform(v_sg.transform(X_sg_n_clean))
+    X_pl_n_clean = preprocess.load_data('plural_n.txt', labels=False)
+    X_pl_n = Binarizer(copy=False).transform(v_pl.transform(X_pl_n_clean))
 
-	X_pl_n_clean = preprocess.load_data('plural_n.txt', labels=False)
-	X_pl_n = Binarizer(copy=False).transform(v_pl.transform(X_pl_n_clean))
+    scores = []
+    n_steps = 100
+    print "size  P\tratio\tsg_err\tpl_err\terror \tsg_std  \tpl_std  \tstd"
+    for train_proportion in np.linspace(0.05, 1, 10):
+        train_size = len(X_sg) * train_proportion
+        steps = [shuffle(X_sg_p, y_sg, X_pl_p, y_pl, n_samples=train_size)
+                 for k in xrange(n_steps)]
+        step_scores = Parallel(n_jobs=-1, verbose=False)(
+            delayed(nouns_score)(*step, X_sg_test=X_sg_n, X_pl_test=X_pl_n)
+            for step in steps)
+        step_scores = np.array(step_scores)
 
-	scores = []
-	n_steps = 100
-	for train_proportion in np.linspace(0.05, 1, 10):
-		train_size = len(X_sg) * train_proportion
-		print "Sampling %d nouns out of the training set..." % train_size
+        score = np.r_[train_size, train_proportion,
+                      step_scores.mean(axis=0),
+                      step_scores.std(axis=0)]
+        print "%d\t%.2f\t%.4f\t%.4f\t%.4f\t%.2e\t%.2e\t%.2e" % tuple(score)
+        scores.append(score)
+    print "Pickling scores..."
 
-		X_sg_steps, y_sg_steps, X_pl_steps, y_pl_steps = zip(*[shuffle(
-			X_sg_p, y_sg, X_pl_p, y_pl, n_samples=train_size)
-			for k in xrange(n_steps)])
-		print "Evaluating %d pairs of classifiers..." % n_steps
-		sg_step_scores = Parallel(n_jobs=-1, verbose=False)(
-			delayed(sg_score)(X_sg_step, y_sg_step, X_sg_n)
-			for X_sg_step, y_sg_step in zip(X_sg_steps, y_sg_steps))
-		pl_step_scores = Parallel(n_jobs=-1, verbose=False)(
-			delayed(pl_score)(X_pl_step, y_pl_step, X_pl_n)
-			for X_pl_step, y_pl_step in zip(X_pl_steps, y_pl_steps))
-		score = (np.mean(sg_step_scores), np.std(sg_step_scores),
-			     np.mean(pl_step_scores), np.std(pl_step_scores))
-		print score
-		scores.append((train_size, train_proportion) + score)
-	print "Pickling scores..."
-	scores = np.array(scores)
-	np.save("train_size", scores)
+    scores = np.array(scores)
+    np.save("train_size", scores)
